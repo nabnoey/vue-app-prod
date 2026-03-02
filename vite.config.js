3286092efa3bf9f61f7ebd4e6fe02f84241a7302
nabnoey <nabnoeyy@gmail.com>
2026-03-02 10:26:40 +0700

    vite.conifg

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 //หรือ base: '/vue-app/prod',
 export default defineConfig({
-  base: '/vue-app/',
+  base: '/vue-app-prod/',
   plugins: [vue()],
   resolve: {
     alias: {
